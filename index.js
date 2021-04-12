2487eedddb62966bfa9b98de99aaa6017e058d8e
Payam Abdullah <payam97abubakr@gmail.com>
2021-04-12 09:48:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names,events) {
+  let messageArray=[];
+  for (let i = 0; i <names.length; i++) {
+    messageArray.push('Thank you, '+names[i]+', for the wonderful ' + events+' gift!');
+  }
+  return messageArray;
+}
+function countDown(i){
+  while (i>=0) {
+    console.log(i);
+    i--;
+  }
+}
+writeCards([ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise');
+countDown(0);
