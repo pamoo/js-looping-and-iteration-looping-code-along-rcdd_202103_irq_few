ccc291a7914508c9850ea928ae58bc91d2bbb5bb
Payam Abdullah <payam97abubakr@gmail.com>
2021-04-12 08:50:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names,events) {
+  let i = 0;
+  for (let i = 0; i <names.length; i++) {
+    console.log(names[i]);
+  }
+  for (let i = 0; i <names.length; i++) {
+    console.log('thank you,' +names[i]+', for the wonderful ' + events+' gift');
+  }
+}
+function countDown(i){
+  while (i>=0) {
+    console.log(i);
+    i--;
+  }
+}
+writeCards([ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise');
+countDown(0);
